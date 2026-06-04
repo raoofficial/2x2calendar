72bc093bd90964d50f9a9d9c8dd4257ec478b9ef
raoofficial <74665771+raoofficial@users.noreply.github.com>
2026-06-05 01:47:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,55 +1,66 @@
-const CACHE_NAME =
-  "calendar-2x2-v1";
-
-const ASSETS = [
-
+const CACHE_NAME = "calendar-2x2-v2";
+const APP_SHELL = [
   "./",
-
   "./index.html",
-
   "./styles.css",
-
   "./app.js",
-
   "./manifest.webmanifest",
-
-  "./utils/calendar.js"
-
+  "./utils/calendar.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/maskable-512.png"
 ];
 
-self.addEventListener(
-  "install",
-  (event) => {
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
 
-    event.waitUntil(
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
 
-      caches.open(CACHE_NAME)
-        .then((cache) => {
-          return cache.addAll(ASSETS);
-        })
+self.addEventListener("fetch", (event) => {
+  const { request } = event;
+  const requestUrl = new URL(request.url);
 
-    );
-
+  if (request.method !== "GET" || requestUrl.origin !== self.location.origin) {
+    return;
   }
-);
 
-self.addEventListener(
-  "fetch",
-  (event) => {
-
+  if (request.mode === "navigate") {
     event.respondWith(
-
-      caches.match(event.request)
-        .then((response) => {
-
-          return (
-            response ||
-            fetch(event.request)
-          );
-
-        })
-
+      fetch(request).catch(() => caches.match("./index.html"))
     );
-
+    return;
   }
-);
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      const networkResponse = fetch(request)
+        .then((response) => {
+          if (response.ok) {
+            event.waitUntil(
+              caches.open(CACHE_NAME).then((cache) => cache.put(request, response.clone()))
+            );
+          }
+
+          return response;
+        })
+        .catch(() => cachedResponse);
+
+      return cachedResponse || networkResponse;
+    })
+  );
+});
